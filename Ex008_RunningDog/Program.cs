ac0fa21bebf559f94c5d4f5147d9cd70ec105bd3
Lexizli <pho_lex-r@yahoo.com>
2022-12-15 18:44:58 +0300

    add running dog

---
+++ Ex008_RunningDog/Program.cs
@@ -0,0 +1,22 @@
+﻿// Two man walk towards from $distance. Funny dog runs from one man to another and back until moment of meeting. 
+// How many times will the dog turn?
+
+static int dogRun(int distance, int firstFriendSpeed, int secondFriendSpeed, int dogSpeed, bool toFirst = false,  int count = 0) 
+{
+    int time = 0;
+    if (distance <= 10) {return count;}
+    if (toFirst) {
+        time = distance / (secondFriendSpeed + dogSpeed);
+    }
+    else
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+    }
+    
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    Console.WriteLine("count = " + count + "  test = " + (2 * (int) (count / 2))); 
+    return dogRun(distance, firstFriendSpeed, secondFriendSpeed, dogSpeed, ! toFirst, count+1);
+}
+
+
+Console.WriteLine(dogRun(100000,1,2,5));
